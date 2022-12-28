7981b8614c102e9fdd479e7413a1d0dc1f0857b4
Tienjui Wang <tian.r.king@gmail.com>
2022-12-28 10:09:25 +0800

    init

---
+++ MDK-ARM/stm32f103c8t6/event_groups.d
@@ -0,0 +1,15 @@
+stm32f103c8t6\event_groups.o: FreeRTOS\Source\event_groups.c
+stm32f103c8t6\event_groups.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+stm32f103c8t6\event_groups.o: .\FreeRTOS\Source\include\FreeRTOS.h
+stm32f103c8t6\event_groups.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+stm32f103c8t6\event_groups.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+stm32f103c8t6\event_groups.o: .\FreeRTOS\Source\include\FreeRTOSConfig.h
+stm32f103c8t6\event_groups.o: .\FreeRTOS\Source\include\projdefs.h
+stm32f103c8t6\event_groups.o: .\FreeRTOS\Source\include\portable.h
+stm32f103c8t6\event_groups.o: .\FreeRTOS\Source\include\deprecated_definitions.h
+stm32f103c8t6\event_groups.o: .\FreeRTOS\Source\portable\RVDS\ARM_CM0\portmacro.h
+stm32f103c8t6\event_groups.o: .\FreeRTOS\Source\include\mpu_wrappers.h
+stm32f103c8t6\event_groups.o: .\FreeRTOS\Source\include\task.h
+stm32f103c8t6\event_groups.o: .\FreeRTOS\Source\include\list.h
+stm32f103c8t6\event_groups.o: .\FreeRTOS\Source\include\timers.h
+stm32f103c8t6\event_groups.o: .\FreeRTOS\Source\include\event_groups.h
